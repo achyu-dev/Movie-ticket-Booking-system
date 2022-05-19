441ec2acd38b4581191fd3fb60b40b1e0b9a6048
ACHYUTH <93060367+achyu2003@users.noreply.github.com>
2022-05-19 06:39:30 +0530

    Add files via upload

---
+++ decelaration.cpp
@@ -0,0 +1,10 @@
+int changeprize(int);
+void reservation(int *,int,int );
+int choice1(void);
+void cancel(int *);
+void records(int);         
+void ticket1(int choice,char name[10],int id2);
+void ticket2(int choice,char name[10],int id2);
+void ticket3(int choice,char name[10],int id2);
+int cmovie(void);
+
---
+++ reverse.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int i,n,r,s=0;
+    
+     
+    printf("\n  Enter The Number:");
+    scanf("%d",&n);
+     
+    //LOOP FOR FINDING THE REVERSE OF A NUMBER
+
