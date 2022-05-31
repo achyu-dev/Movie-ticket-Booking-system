0632644a3d76ae615dbe498b06c0bc2df2acaaac
ACHYUTH <93060367+achyu2003@users.noreply.github.com>
2022-05-31 23:40:04 +0530

    Update decleration.c

--- decleration.c
+++ decleration.c
@@ -2,12 +2,16 @@ int changeprize(int);
 void reservation(int *,int,int );
 int choice1(void);
 void cancel(int *);
-void ticket1(int choice,char name[10],int id2,int price);
-void ticket2(int choice,char name[10],int id2,int price);
-void ticket3(int choice,char name[10],int id2,int price);
-void ticket4(int choice,char name[10],int id2,int price);
-
-
+void ticket1(int choice,char name[100],int id2,int price);
+void ticket2(int choice,char name[100],int id2,int price);
+void ticket3(int choice,char name[100],int id2,int price);
+void ticket4(int choice,char name[100],int id2,int price);
+void ticket5(int choice,char name[100],int id2,int price);
+void ticket6(int choice,char name[100],int id2,int price);
+void ticket7(int choice,char name[100],int id2,int price);
+void ticket8(int choice,char name[100],int id2,int price);
+void ticket9(int choice,char name[100],int id2,int price);
+void ticket10(int choice,char name[100],int id2,int price);
 int cmovie(void);
 int movie(void);
 void details(void);
